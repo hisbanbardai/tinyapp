58bb8c3251037653c54a3736687a70f4da06fc65
Hisban Shiraz <hisbanbardai@gmail.com>
2023-10-01 16:45:39 -0600

    Handle edge case if url does not exist in the url database

--- express_server.js
+++ express_server.js
@@ -8,16 +8,16 @@ app.set("view engine", "ejs");
 //body parser library to convert request body from Buffer to string
 app.use(express.urlencoded({ extended: true }));
 
+//generate string for shortURL
 function generateRandomString() {
   //toString(36) to convert it into base 36 (26 char + 0-9)
-  return Math.random().toString(36).slice(2,8);
+  return Math.random().toString(36).slice(2, 8);
 }
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,27 +34,40 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new.ejs");
-})
+});
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render("urls_show", templateVars);
-})
+  //if id does not exist
+  if (!urlDatabase[req.params.id]) {
+    res.send("Does not exist in the urlDatabase.");
+  } else {
+    const templateVars = {
+      id: req.params.id,
+      longURL: urlDatabase[req.params.id],
+    };
+    res.render("urls_show", templateVars);
+  }
+});
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   //adding shortURL to urlDatabase
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
-})
+});
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  //if id does not exist
+  if (!urlDatabase[req.params.id]) {
+    res.send("Does not exist in the urlDatabase.");
+  } else {
+    const longURL = urlDatabase[req.params.id];
+    res.redirect(longURL);
+  }
 });
 
 app.listen(PORT, () => {
