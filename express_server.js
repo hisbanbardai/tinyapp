92c8457618cd30a10fbdba75cf6d7a62b0f54170
Hisban Shiraz <hisbanbardai@gmail.com>
2023-10-01 15:35:42 -0600

    Add generated shortURL as key and its value as longURL obtained from form submission to urlDatabase

--- express_server.js
+++ express_server.js
@@ -46,8 +46,10 @@ app.get("/urls/:id", (req, res) => {
 })
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  // console.log(req.body, generateRandomString()); // Log the POST request body to the console
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  res.send(urlDatabase);
 })
 
 app.listen(PORT, () => {
