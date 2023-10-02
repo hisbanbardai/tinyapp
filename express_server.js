d2a52278950ab7c109e1357602fe2152636c2331
Hisban Shiraz <hisbanbardai@gmail.com>
2023-10-02 13:37:20 -0600

    Add status 404 when id does not exist

--- express_server.js
+++ express_server.js
@@ -43,7 +43,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   //if id does not exist
   if (!urlDatabase[req.params.id]) {
-    res.send("Does not exist in the urlDatabase.");
+    res.status(404).send("Does not exist in the urlDatabase.");
   } else {
     const templateVars = {
       id: req.params.id,
@@ -63,7 +63,7 @@ app.post("/urls", (req, res) => {
 app.get("/u/:id", (req, res) => {
   //if id does not exist
   if (!urlDatabase[req.params.id]) {
-    res.send("Does not exist in the urlDatabase.");
+    res.status(404).send("Does not exist in the urlDatabase.");
   } else {
     const longURL = urlDatabase[req.params.id];
     res.redirect(longURL);
